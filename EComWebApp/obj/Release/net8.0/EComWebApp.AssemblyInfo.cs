e6b5f537f947095c6160d387d42c332dc6334a9b
Gopi <kkgopinth1@gmail.com>
2025-07-15 19:39:06 +0530

    second commit

--- EComWebApp/obj/Release/net8.0/EComWebApp.AssemblyInfo.cs
+++ EComWebApp/obj/Release/net8.0/EComWebApp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("EComWebApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e6b3f77d6062f1fd1d7e2896f2a39824a2b22ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("EComWebApp")]
